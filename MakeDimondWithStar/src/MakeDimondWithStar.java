51268c59432506341775c2fbc84dc7a43cdbf976
Aslıhan Şimşek <simseekasli@gmail.com>
2022-06-11 15:58:24 +0300

    -Take number from user -Create Diamond with star

---
+++ MakeDimondWithStar/src/MakeDimondWithStar.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MakeDimondWithStar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please Enter a Number ..:");
+        int number = scanner.nextInt();
+
+
+        for (int i = 0; i <= number; i++) {
+            for (int j = 0; j < (number - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = number -1 ; i >= 0; i--) {
+            for (int j =  (number - i); j > 0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i + 1); k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
